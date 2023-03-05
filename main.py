74370576693872c782ed18058d99204d63f13688
Andrei-Cătălin Mușat <123704487+andrei30musat@users.noreply.github.com>
2023-03-06 00:32:23 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,37 @@
-board = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
+board =[
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7]
 ]
 
-def print_board(board):
-    for i in range(len(board[0])):
-        if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - ")
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
 
-        for j in range(len(board[0])):
-            if j % 3 == 0 and j != 0:
-                print("| ", end="")
+    for i in range(1, 10):
+        if valid(board, i, row, col):
+            board[row][col] = i
 
-            if j == 8:
-                print(board[i][j])
-            else:
-                print(board[i][j] , end=" ")
+            if solve(board):
+                return True
 
+            board[row][col] = 0
 
-def find_empty(board):
-    for i in range(len(board[0])):
-        for j in range(len(board[0])):
-            if board[i][j] == 0:
-                return (i, j)  # linia si coloana
+    return False
+
 
 def valid(board, num, row, col):
-    #num e numarul pe care vrem sa l introducem
-    #(row,col) reprezinta coordonatele numarului pe care vrem sa l introducem id est num(row, col)
+    # num e numarul pe care vrem sa l introducem
+    # (row,col) reprezinta coordonatele numarului pe care vrem sa l introducem id est num(row, col)
 
     # Verificam daca num este deja pe linia unde vrem sa l introducem
     for j in range(len(board[0])):
@@ -49,32 +47,37 @@ def valid(board, num, row, col):
     box_x = col // 3
     box_y = row // 3
 
-    for i in range(box_y*3, box_y*3 + 3):
-        for j in range(box_x * 3, box_x*3 + 3):
-            if board[i][j] == num and (i,j) != (row,col):
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num and (i, j) != (row, col):
                 return False
 
     return True
 
-def solve(board):
-    find = find_empty(board)
-    if not find:
-        return True
-    else:
-        row, col = find
 
-    for i in range(1,10):
-        if valid(board, i, row, col ):
-            board[row][col] = i
+def print_board(board):
+    for i in range(len(board[0])):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - ")
 
-            if solve(board):
-                return True
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print("| ", end="")
 
-            board[row][col] = 0
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(board[i][j], end=" ")
 
-    return False
+
+def find_empty(board):
+    for i in range(len(board[0])):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)  # linia si coloana
+
 
 print_board(board)
-print("=====================")
 solve(board)
+print("=====================")
 print_board(board)
